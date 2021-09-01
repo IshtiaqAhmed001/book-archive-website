00f54d2f87f21b7befc1c1cf2827c1e500ad0ffd
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-09-02 02:55:34 +0600

    loaded data again

--- js/app.js
+++ js/app.js
@@ -7,25 +7,9 @@ const loadBooks = () => {
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayBooks(data));
+        .then(data => console.log(data));
 }
 
 const displayBooks = data => {
     console.log(data);
 }
-
-    // const bookContainer = document.getElementById('result-container');
-    // const bookDetails = document.createElement('div');
-    // bookDetails.classList.add('col');
-    // bookDetails.innerHTML = `
-    //     <div class="card">
-    //         <img src="..." class="card-img-top" alt="...">
-    //         <div class="card-body">
-    //             <h5 class="card-title">Card title</h5>
-    //             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-    //                 to additional content. This content is a little bit longer.</p>
-    //         </div>
-    //     </div>
-    // `;
-    // bookContainer.appendChild(bookDetails);
-
