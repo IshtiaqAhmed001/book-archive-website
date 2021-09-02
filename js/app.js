ed609ae32df2dd2cddf9a35e44c30929eb924648
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-09-02 11:53:51 +0600

    book name author name displays

--- js/app.js
+++ js/app.js
@@ -7,9 +7,30 @@ const loadBooks = () => {
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayBooks(data));
 }
 
 const displayBooks = data => {
     console.log(data);
+    const resultContainer = document.getElementById('result-container');
+    const books = data.docs.slice(0, 50);
+    books.forEach(book => {
+        // console.log(book);
+        const newItem = document.createElement('div');
+        newItem.classList.add('col');
+        newItem.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <p class="card-text">
+                <h6>Author Name:${book.author_name}</h6>
+
+                </p>
+            </div>
+        </div>
+        `;
+        resultContainer.appendChild(newItem);
+    })
+
 }
