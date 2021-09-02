6a3a0346f58bde6fd92a8d5e892ea1683b518912
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-09-02 12:19:46 +0600

    added image

--- js/app.js
+++ js/app.js
@@ -13,7 +13,15 @@ const loadBooks = () => {
 const displayBooks = data => {
     console.log(data);
     const resultCount = document.getElementById('total-result-found');
-    resultCount.innerText = `About ${data.numFound} results found!`;
+
+    // show search result count
+    if (data.numFound === 0) {
+        resultCount.innerText = `No results found!`;
+    }
+    else {
+        resultCount.innerText = `About ${data.numFound} results found!`;
+    }
+
     const resultContainer = document.getElementById('result-container');
     // clearing previous search results 
     resultContainer.textContent = '';
@@ -23,9 +31,10 @@ const displayBooks = data => {
         // console.log(book);
         const newItem = document.createElement('div');
         newItem.classList.add('col');
+        const imageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
         newItem.innerHTML = `
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${imageUrl}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <p class="card-text">
