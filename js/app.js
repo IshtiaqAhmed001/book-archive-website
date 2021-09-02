3859297c0b42f75dfcc267eccd16680ea542304a
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-09-02 12:07:54 +0600

    total result count added

--- js/app.js
+++ js/app.js
@@ -12,7 +12,12 @@ const loadBooks = () => {
 
 const displayBooks = data => {
     console.log(data);
+    const resultCount = document.getElementById('total-result-found');
+    resultCount.innerText = `About ${data.numFound} results found!`;
     const resultContainer = document.getElementById('result-container');
+    // clearing previous search results 
+    resultContainer.textContent = '';
+    // slicing the array for showing 50 items in result
     const books = data.docs.slice(0, 50);
     books.forEach(book => {
         // console.log(book);
@@ -25,6 +30,8 @@ const displayBooks = data => {
                 <h5 class="card-title">${book.title}</h5>
                 <p class="card-text">
                 <h6>Author Name:${book.author_name}</h6>
+                <h6>Publisher:${book.publisher}</h6>
+                <h6>First Publish year:${book.first_publish_year}</h6>
 
                 </p>
             </div>
